7f82f07b24997fd01bad76373011e80087e5d6a8
vishwajit01072005 <144530712+vishwajit01072005@users.noreply.github.com>
2023-10-19 11:02:36 +0530

    codes

---
+++ vishwajit.py
@@ -0,0 +1,18 @@
+a=[]
+b=()
+c={"key":"value"}
+dictionary={"name":"vishwajit","age":"18"}
+dictionary["DOB"]="1-july-2005"
+print(dictionary)
+# print(type(dictionary))
+# print(dictionary.key())
+# print(dictionary.value())
+ 
+# for x,y in dictionary.items():
+#     print(x,y)
+
+# for a in dictionary.keys():
+#     print(a)
+
+# for b in dictionary.values():
+#     print(b)
