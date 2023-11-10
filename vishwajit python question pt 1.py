eadac362bd601b27b64aa47750131d34e141f85d
vishwajit01072005 <144530712+vishwajit01072005@users.noreply.github.com>
2023-11-10 23:44:26 +0530

    Add files via upload
    some basic python codes of creating a function using (def)

---
+++ vishwajit python question pt 1.py
@@ -0,0 +1,105 @@
+#Q1]
+# a=[]
+# b=()
+# c={"key":"value"}
+# dictionary={"name":"vishwajit","age":"18"}
+# dictionary["DOB"]="1-july-2005"
+# print(dictionary)
+# print(type(dictionary))
+# print(dictionary.key())
+# print(dictionary.value())
+ 
+# for x,y in dictionary.items():
+#     print(x,y)
+
+# for a in dictionary.keys():
+#     print(a)
+
+# for b in dictionary.values():
+#     print(b)
+
+
+
+# Q2]
+# dictionary={}
+# while True:
+    
+#     key=input("enter any key:")
+#     value=input("enter any value")
+#     dictionary[key]=value
+#     stop=input("do you want to continue type yes/no")
+#     if stop!="yes":
+#         break
+
+# print(dictionary)
+
+
+
+#Q3]Create a function to check wether a number if is less than 10 or not
+
+# def number(x):
+#     if x < 10:
+#         print("True")
+#     else:
+#         print("False")
+# print(number(5))
+# print(number(50))
+
+
+
+#Q4]create a function to reverse a string
+        
+# def string(v):
+#     print(v[::-1])
+# print(string("vishwajit"))
+
+
+
+#Q5]Create a function to print even number upto n
+
+# def prime(n):
+#     for i in range(2,n+1,2):
+#         print(i)
+# print(prime(90))
+
+
+#Q6] Create a funtion to check whether the person is eligible for voting 
+
+# def voting(n):
+#     if n>18:
+#         print("you are eligible for voting")
+#     else:
+#         print("you are not eligible for voting")
+# print(voting(50))
+# print(voting(4))             
+
+
+# Q7]Create a function to find largest among 3 numbers 
+
+
+# def largest(p,q,r):
+#     if p>q and q>r:
+#         print(p)
+#     elif q>p and q>r:
+#         print(Q)
+#     else:
+#         print(r)
+# print(largest(90000,35764,2)) 
+
+
+#Q8]Create a function to print a table of a number 
+
+
+# def table(n):
+#     for i in range (1,11):
+#         print(n*i)
+# print(table(1))
+
+
+#Q9]L=[3,44,78,22,4];create a function to sum all numbers given the list
+
+
+# def list(a,b,c,d,e):
+#     sum=a+b+c+d+e
+#     print(sum)
+# print(list(3,44,78,22,4))    
